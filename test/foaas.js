5042e1f2373a907e86fb4af827674b7ef3554179
Hendri Pretorius <pretorh@gmail.com>
2013-10-13 15:53:44 +0200

    request error handling return request error events in callback

--- test/foaas.js
+++ test/foaas.js
@@ -7,8 +7,20 @@ function simple() {
             assert.fail(err);
         } else {
             assert.equal("Fuck off, Tom. - Chris", text);
+            console.log("ok: simple");
+        }
+    });
+}
+
+function exceptionHandling() {
+    sc.get("not really a url", function(err, text) {
+        if (err) {
+            console.log("ok: expect error");
+        } else {
+            assert.fail("no error returned");
         }
     });
 }
 
 process.nextTick(simple);
+process.nextTick(exceptionHandling);
