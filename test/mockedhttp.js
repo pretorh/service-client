00364aa73b18ea7bf5ea1e438884bbf62f95cb60
Hendri Pretorius <pretorh@gmail.com>
2013-10-19 17:09:01 +0200

    mock builder http mock builder can overwrite status code return fix null data return issue when creating buffer

--- test/mockedhttp.js
+++ test/mockedhttp.js
@@ -2,14 +2,17 @@ var assert = require("assert"),
     sc = require("../");
 
 function mockedHttp() {
+    var builder = new sc.mocks.Builder({
+        data: "result from mock"
+    });
+    
     sc.get("random url", {
-        _http: function(url, callback) {
-            return new sc.mocks.RequestMock(callback, new sc.mocks.ResponseMock("result from mock"));
-        }
+        _http: builder.httpRequest
     },
     function(err, data) {
         if (err) {
-            assert.fail(err);
+            console.log(err);
+            assert.fail("mockedHttp");
         } else {
             assert.equal("result from mock", data);
             console.log("ok: mockedHttp");
@@ -17,4 +20,26 @@ function mockedHttp() {
     });
 };
 
+function overwriteStatusCode() {
+    var builder = new sc.mocks.Builder({
+        statusCode: 399
+    });
+    
+    sc.get("random url", {
+        _http: builder.httpRequest
+    },
+    function(err, data) {
+        if (err) {
+            //assert.equal();
+            assert.equal("non 2xx status code: 399", err.error.message);
+            assert.equal("response", err.action);
+            console.log("ok: overwriteStatusCode");
+            
+        } else {
+            assert.fail("overwriteStatusCode");
+        }
+    });
+};
+
 process.nextTick(mockedHttp);
+process.nextTick(overwriteStatusCode);
