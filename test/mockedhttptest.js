a1ab2dadf108a126c9e34ba37ad8a2e39d27d8ca
Hendri Pretorius <pretorh@gmail.com>
2013-10-19 18:03:12 +0200

    https support use https/http depending on detected protocol / option

--- test/mockedhttptest.js
+++ test/mockedhttptest.js
@@ -41,5 +41,46 @@ function overwriteStatusCode() {
     });
 };
 
+function canDetectAndUseHttps() {
+    var builder = new sc.mocks.Builder({
+        data: "result from mock"
+    });
+    
+    sc.get("https://www.google.com", {
+        _https: builder.httpRequest
+    },
+    function(err, data) {
+        if (err) {
+            console.log(err);
+            assert.fail("canDetectAndUseHttps");
+        } else {
+            assert.equal("result from mock", data);
+            console.log("ok: canDetectAndUseHttps");
+        }
+    });
+};
+
+function canOverwriteProtocol() {
+    var builder = new sc.mocks.Builder({
+        data: "result from mock"
+    });
+    
+    sc.get("www.google.com", {
+        _https: builder.httpRequest,
+        protocol: "https"
+    },
+    function(err, data) {
+        if (err) {
+            console.log(err);
+            assert.fail("canOverwriteProtocol");
+        } else {
+            assert.equal("result from mock", data);
+            console.log("ok: canOverwriteProtocol");
+        }
+    });
+};
+
 process.nextTick(mockedHttp);
 process.nextTick(overwriteStatusCode);
+process.nextTick(canDetectAndUseHttps);
+process.nextTick(canOverwriteProtocol);
